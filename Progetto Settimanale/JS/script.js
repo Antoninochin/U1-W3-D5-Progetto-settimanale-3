5585b6aa78fa28876ecbaefffda0dff8bcc10432
Antoninochin <antonino_chindemi@virgilio.it>
2022-09-16 13:48:11 +0200

    modificato js result

--- Progetto Settimanale/JS/script.js
+++ Progetto Settimanale/JS/script.js
@@ -2,10 +2,10 @@ function Write(number){
     document.getElementById('display').value += number
 }
 
-/*function Result(){
+function Result(){
     let result = eval(document.getElementById('display').value)
     document.getElementById('display').value = result
-}*/
+}
 
 function Delete(){
     let c = document.getElementById('display').value
@@ -15,10 +15,10 @@ function Delete(){
     document.getElementById('display').value = ''
  }
 
- //risultato senza il metodo eval
+ /*risultato senza il metodo eval
   function Result(){
   let somma = [1,2,3,4,5,6,7,8,9]
   let Result = somma(getElementById('display').value)
   document.getElementById('display').value += Result.substring(0, Result.length-1)
-}
+}*/
   
