32ce680f59ab6323632cc0315d8a5dc4009795f7
Antoninochin <antonino_chindemi@virgilio.it>
2022-09-16 13:11:13 +0200

    Progetto

---
+++ Progetto Settimanale/JS/script.js
@@ -0,0 +1,24 @@
+function Write(number){
+    document.getElementById('display').value += number
+}
+
+/*function Result(){
+    let result = eval(document.getElementById('display').value)
+    document.getElementById('display').value = result
+}*/
+
+function Delete(){
+    let c = document.getElementById('display').value
+    document.getElementById('display').value = c.substring(0, c.length-1)
+}
+ function Reset(){
+    document.getElementById('display').value = ''
+ }
+
+ //risultato senza il metodo eval
+  function Result(){
+  let somma = [1,2,3,4,5,6,7,8,9]
+  let Result = somma(getElementById('display').value)
+  document.getElementById('display').value += Result.substring(0, Result.length-1)
+}
+  
